35223b06c7b99885c5e00ac1f6fade0d76e54732
rajesh <rjmathew@gmali.com>
2017-06-20 07:00:20 -0700

    Adding Autowired ref to java class

--- src/main/java/com/disney/studios/kotlin_java/controller/test.kt
+++ src/main/java/com/disney/studios/kotlin_java/controller/test.kt
@@ -1,12 +1,17 @@
 package com.disney.studios.kotlin_java.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpRequest
 import org.springframework.web.HttpRequestHandler
 import javax.servlet.http.HttpServletRequest
 
 class test(val request: HttpServletRequest)  {
+
+    @Autowired
+    lateinit var testService : TestService
+
     fun getGreeting() : String{
-        var x = TestService().getHelloMessage(request)
+        var x = testService.getHelloMessage(request)
         println(x)
         x = x + " " + request.method
         return x
